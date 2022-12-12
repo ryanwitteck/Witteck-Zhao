f7e21223d1b6b428ec1614534ab104db1265ee3a
zhaoChar <zhao.char@northeastern.edu>
2022-12-12 17:41:01 -0500

    added routes documentation

--- flask-app/app.py
+++ flask-app/app.py
@@ -6,6 +6,38 @@
 # that lives in src/__init__.py
 from src import create_app
 
+'''
+    Routes:
+
+    products/                   GET
+    products/browse             GET
+    products/unapproved         GET
+    products/<pid>              GET
+    products/<pid>/categories   GET
+    products/<pid>/reviews      GET
+    products/<pid>/sales-data   GET
+    products/add-product        POST
+    products/change-<param>     POST
+    products/approve-product    POST
+    products/unapprove-product  POST
+
+    customers/                  GET
+    customers/<cid>             GET
+    customers/<cid>/invoices    GET
+    customers/<cid>/reviews     GET
+    customers/post-review       POST
+
+    suppliers/                  GET
+    suppliers/<sid>             GET
+    suppliers/<sid>/products    GET
+    suppliers/<sid>/company     GET
+    suppliers/<sid>/sales-data  GET
+
+    users/customers/iem-map     GET
+    users/suppliers/iem-map     GET
+    users/sales-rep/iem-map     GET
+'''
+
 # create the app object
 app = create_app()
 
